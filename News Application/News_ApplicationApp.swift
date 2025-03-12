c73250ebd0bce83b504b13796539c65ba9095023
GujMeister <90295019+GujMeister@users.noreply.github.com>
2025-03-12 16:59:00 +0400

    Initial Commit

---
+++ News Application/News_ApplicationApp.swift
@@ -0,0 +1,17 @@
+//
+//  News_ApplicationApp.swift
+//  News Application
+//
+//  Created by Luka Gujejiani on 12.03.25.
+//
+
+import SwiftUI
+
+@main
+struct News_ApplicationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
