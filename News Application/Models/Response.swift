ffb13f6268dc7b9efb5e6e61348d8b9142697ab1
LukaGujejiani <luka.gujejiani@space.ge>
2025-04-17 17:03:42 +0400

    Update Response.swift

--- News Application/Models/Response.swift
+++ News Application/Models/Response.swift
@@ -9,25 +9,13 @@ import Foundation
 
 // MARK: - Response
 struct Response: Decodable {
-    let status: String?
-    let totalResults: Double?
     let articles: [Article]
-}
-
-// MARK: - Article
-struct Article: Decodable {
-    let source: Source?
-    let author: String?
-    let title: String?
-    let description: String?
-    let url: String?
-    let urlToImage: String?
-    let publishedAt: String?
-    let content: String?
-}
-
-// MARK: - Source
-struct Source: Decodable {
-    let id: String?
-    let name: String?
+    
+    struct Article: Decodable {
+        let author: String?
+        let title: String?
+        let urlToImage: String?
+        let publishedAt: String?
+        let content: String?
+    }
 }
