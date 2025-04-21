a3fc206df1e028df11e2ec33d3b39b3db9bb86de
LukaGujejiani <luka.gujejiani@space.ge>
2025-04-21 20:37:00 +0400

    Added DetailsView and DetailsViewModel

---
+++ News Application/NA_Presentation/Details/DetailsView.swift
@@ -0,0 +1,18 @@
+//
+//  DetailsView.swift
+//  News Application
+//
+//  Created by Luka Gujejiani on 21.04.25.
+//
+
+import SwiftUI
+
+struct DetailsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DetailsView()
+}
---
+++ News Application/NA_Presentation/Details/DetailsViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  DetailsViewModel.swift
+//  News Application
+//
+//  Created by Luka Gujejiani on 21.04.25.
+//
+
+import Foundation
